3fdaf2e7e3c824fff462b05f74c6d26ca1367338
jehes <156023992+Jehes@users.noreply.github.com>
2024-07-20 15:11:48 +0300

    requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -2,12 +2,10 @@
 
 python -c "import pip" > nul 2>&1
 if %errorlevel% neq 0 (
-    echo pip is not installed. Installing pip...
     python -m ensurepip
 )
 python -c "import requests; import beautifulsoup4" > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Required libraries are not installed. Installing...
     pip install requests 
     pip install beautifulsoup4
 )
